2d792e14f73363f89fc57a88e00d9c23683330ff
unknown <ogencay98@gmail.com>
2019-02-01 02:51:42 +0300

    vol1.6

--- api/dbconfig.php
+++ api/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_USER', "bde113a49769a2");
-define('DB_PASSWORD', "b582a1de");
+define('DB_USER', "b2ace13c630076");
+define('DB_PASSWORD', "0fcddcf2");
 define('DB_SERVER' , "us-cdbr-iron-east-01.cleardb.net");
-define('DB_DATABASE', "heroku_5bf1bf6802d8332");
+define('DB_DATABASE', "heroku_69215c357f83bb2");
 ?>
