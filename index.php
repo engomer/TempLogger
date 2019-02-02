bbe03d41a572b5caaf11e78138ba386bd94fd959
engomer <ogencay98@gmail.com>
2019-02-02 14:06:40 +0300

    vol1.8

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $result = mysqli_query($connect, $query);
 $chart_data = '';
 while($row = mysqli_fetch_array($result))
 {
-	$end_date = date("Y-m-d h:m:s", strtotime("+" . 3 . " days", $row["timestamp"]));
+	$end_date = date("Y-m-d h:m:s", strtotime("+" .3. " days", strtotime($row["timestamp"])));
  	$chart_data .= "{ timestamp:'".$end_date."', tempin:".$row["tempin"].", tempout:".$row["tempout"].", hum:".$row["hum"]."}, ";
 }
 $chart_data = substr($chart_data, 0, -2);
