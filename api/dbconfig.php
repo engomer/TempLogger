a157ed407a5a7ab613fd26ed9a405424c2129aec
engomer <43203464+engomer@users.noreply.github.com>
2019-03-28 15:01:00 +0300

    Update dbconfig.php

--- api/dbconfig.php
+++ api/dbconfig.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_USER', "b2ace13c630076");
-define('DB_PASSWORD', "0fcddcf2");
-define('DB_SERVER' , "us-cdbr-iron-east-01.cleardb.net");
-define('DB_DATABASE', "heroku_69215c357f83bb2");
-define('DB_TABLE', "log");
+define('DB_USER', "USERNAME");
+define('DB_PASSWORD', "PASSWORD");
+define('DB_SERVER' , "SERVER");
+define('DB_DATABASE', "DATABASE NAME");
+define('DB_TABLE', "TABLE NAME");
 ?>
