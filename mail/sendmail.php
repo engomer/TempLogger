81c1f916adfa9b21dcf5bcd1fc1d52c15ca2ef1d
engomer <ogencay98@gmail.com>
2019-05-21 01:08:24 +0300

    mail

--- mail/sendmail.php
+++ mail/sendmail.php
@@ -4,9 +4,9 @@ $mail = new PHPMailer(); // create a new object
 $mail->IsSMTP(); // enable SMTP
 $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 $mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for GMail
 $mail->Host = "smtp.gmail.com";
-$mail->Port = 465; // or 587
+$mail->Port = 587; // or 587
 $mail->IsHTML(true);
 $mail->SetLanguage("tr", "phpmailer/language");
 $mail->CharSet  ="utf-8";
