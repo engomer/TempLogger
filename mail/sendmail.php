c5655c62b322e4e450886c0eae307562ae2c86b7
engomer <ogencay98@gmail.com>
2019-05-21 01:21:41 +0300

    mail

--- mail/sendmail.php
+++ mail/sendmail.php
@@ -4,9 +4,9 @@ $mail = new PHPMailer(); // create a new object
 $mail->IsSMTP(); // enable SMTP
 $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 $mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for GMail
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
 $mail->Host = "smtp.gmail.com";
-$mail->Port = 587; // or 587
+$mail->Port = 465; // or 587
 $mail->IsHTML(true);
 $mail->SetLanguage("tr", "phpmailer/language");
 $mail->CharSet  ="utf-8";
@@ -15,15 +15,19 @@ $mail->Username = "omergsensor@gmail.com"; // Mail adresi
 $mail->Password = "hdxomaxafdyljaki"; // Parola
 $mail->SetFrom("omergsensor@gmail.com", "ÖmerG"); // Mail adresi
 
-$mail->AddAddress("ogencay98@gmail.com"); // Gönderilecek kişi
+if(isset($_GET['mailto']) && isset($_GET['subject']) && isset($_GET['body'])){ 
+$mailto = $_GET['mailto'];
+$subject = $_GET['subject'];
+$body = $_GET['body'];
+$mail->AddAddress($mailto); // Gönderilecek kişi
 
-$mail->Subject = "Sideden Gönderildi";
-$mail->Body = "$name<br />$email<br />$subject<br />$message";
+$mail->Subject = $subject;
+$mail->Body = "$name<br />$email<br />$subject<br />$body";
 
 if(!$mail->Send()){
                 echo "Mailer Error: ".$mail->ErrorInfo;
 } else {
                 echo "Message has been sent";
 }
-
+}
 ?>
