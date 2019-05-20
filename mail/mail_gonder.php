5b41daa5df0f32b3b03c00511532a88e25159598
engomer <ogencay98@gmail.com>
2019-05-21 00:55:35 +0300

    mail

---
+++ mail/mail_gonder.php
@@ -0,0 +1,29 @@
+<?php
+require("class.phpmailer.php");
+$mail = new PHPMailer(); // create a new object
+$mail->IsSMTP(); // enable SMTP
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 465; // or 587
+$mail->IsHTML(true);
+$mail->SetLanguage("tr", "phpmailer/language");
+$mail->CharSet  ="utf-8";
+
+$mail->Username = "omergsensor@gmail.com"; // Mail adresi
+$mail->Password = "15101998omer"; // Parola
+$mail->SetFrom("omergsensor@gmail.com", "ÖmerG"); // Mail adresi
+
+$mail->AddAddress("ogencay98@gmail.com"); // Gönderilecek kişi
+
+$mail->Subject = "Sideden Gönderildi";
+$mail->Body = "$name<br />$email<br />$subject<br />$message";
+
+if(!$mail->Send()){
+                echo "Mailer Error: ".$mail->ErrorInfo;
+} else {
+                echo "Message has been sent";
+}
+
+?>
