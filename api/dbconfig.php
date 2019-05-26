dcd1a59338cbf65796d0641623edba724682cfa6
engomer <43203464+engomer@users.noreply.github.com>
2019-05-26 18:42:39 +0300

    Update dbconfig.php

--- api/dbconfig.php
+++ api/dbconfig.php
@@ -1,13 +1,8 @@
 <?php
-<<<<<<< HEAD
-define('DB_USER', "DATABASE USERNAME");
-define('DB_PASSWORD', "DATABASE PASSWORD");
-define('DB_SERVER' , "DATABASE SERVER");
-=======
 define('DB_USER', "USERNAME");
 define('DB_PASSWORD', "PASSWORD");
 define('DB_SERVER' , "SERVER");
->>>>>>> 516dc191250a61fe6cc9f2fb60d8fd35f50fc8d5
+
 define('DB_DATABASE', "DATABASE NAME");
 define('DB_TABLE', "TABLE NAME");
 ?>
